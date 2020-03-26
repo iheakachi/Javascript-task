033005e6b3611b086779839c100273c3f1770c8e
iheakachi nwabueze <lindanwabueze09@gmail.com>
2020-03-26 23:34:01 +0100

    javascript task

---
+++ script.js
@@ -0,0 +1,20 @@
+< script >
+    var name = "Iheakachi";
+var courses = ["HTML", "CSS", "JavaScript", "Go"];
+if ((courses.length % 2) == 0) {
+    // even number
+    for (var i = 1; i <= 200; i = i + 1) {
+        if (i % 2 == 0) {
+            document.write(i);
+            document.write("</br>");
+        }
+    }
+} else {
+    for (var i = 1; i <= 200; i = i + 1) {
+        if (i % 2 != 0) {
+            document.write(i);
+            document.write("</br>");
+        }
+    }
+} <
+/script>
